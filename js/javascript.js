a52406dca9750c8c7da77f134be4ffd51e1fe608
sapucci16 <sa.pucci20@gmail.com>
2022-10-03 14:48:40 +0200

    Agregada funcionalidad al boton

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,13 @@
+var color = false; // false es negro, true es rojo
+
 function comenzar(){
-    document.getElementsByTagName("body")[0].style.backgroundColor = "red";
+
+    if (!color){
+        document.getElementsByTagName("body")[0].style.backgroundColor = "red";
+        color = true;
+    } else {
+        document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+        color = false;
+    }
+    
 }
